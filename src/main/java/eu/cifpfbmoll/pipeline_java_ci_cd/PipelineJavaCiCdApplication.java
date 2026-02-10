1476278c1fe6c277cbe0a4a218312029d386c21a
Edwin Espinoza <edwin.smx2@gmail.com>
2026-02-10 21:08:06 +0100

    Inicialización del proyecto con Spring Initializr

---
+++ src/main/java/eu/cifpfbmoll/pipeline_java_ci_cd/PipelineJavaCiCdApplication.java
@@ -0,0 +1,13 @@
+package eu.cifpfbmoll.pipeline_java_ci_cd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineJavaCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelineJavaCiCdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/cifpfbmoll/pipeline_java_ci_cd/PipelineJavaCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.cifpfbmoll.pipeline_java_ci_cd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineJavaCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
