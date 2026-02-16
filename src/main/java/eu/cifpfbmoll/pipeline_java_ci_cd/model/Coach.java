8c36e8647f3c97c044af4432c5343bab95a1d303
espinozam <edwin.smx2@gmail.com>
2026-02-16 03:40:28 +0100

    Implementar entidad Coach con anotaciones JPA y validación de campos

---
+++ src/main/java/eu/cifpfbmoll/pipeline_java_ci_cd/model/Coach.java
@@ -0,0 +1,34 @@
+package eu.cifpfbmoll.pipeline_java_ci_cd.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "coaches")
+public class Coach {
+
+    // atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String firstName;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String lastName;
+
+    @Email
+    @NotBlank
+    @Column(nullable = false, unique = true)
+    private String email;
+}
